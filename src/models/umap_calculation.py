d82f8998d6b05714f9425cece0c59a5ed55e0d0b
Julian Lehrer <jmlehrer@ucsc.edu>
2021-11-08 14:32:17 -0800

    Bugfixes, added dist options to umap calculations

--- src/models/umap_calculation.py
+++ src/models/umap_calculation.py
@@ -7,7 +7,8 @@ import dask.dataframe as dd
 import boto3
 import matplotlib.pyplot as plt 
 import seaborn as sns
-    
+import itertools 
+
 s3 = boto3.resource(
     's3',
     endpoint_url="https://s3.nautilus.optiputer.net",
@@ -15,16 +16,6 @@ s3 = boto3.resource(
     aws_secret_access_key="DRXgeKsTLctfFX9udqfT04go8JpxG3qWxj0OKHVU",
 )
 
-def calc_umap(data, n_neighbors=15, min_dist=0.1, n_components=2, metric='euclidean'):
-    fit = umap.UMAP(
-        n_neighbors=n_neighbors,
-        min_dist=min_dist,
-        n_components=n_components,
-        metric=metric,
-        verbose=True,
-    )
-    return fit.fit_transform(data);
-    
 def upload(file_name, remote_name):
     s3.Bucket('braingeneersdev').upload_file(
         Filename=file_name,
@@ -46,7 +37,7 @@ def plot_umap(umap_data, title):
 
     plt.title(title)
     fig.savefig(f'{title}.png')
-    
+
 here = pathlib.Path(__file__).parent.absolute()
 
 print('Reading in organoid data')
@@ -65,21 +56,25 @@ print('Joining DataFrames')
 comb = pd.concat([df_organoid_raw, df_primary_raw])
 
 params = {
-    'n_neighbors': [1000, comb.shape[0]//4, comb.shape[0]//3],
+    'n_neighbors': [15, 100, 1000, comb.shape[0]//4, comb.shape[0]//3],
     'min_dist': [0.1, 0.25, 0.5, 0.8, 0.99],
     'n_components': [2, 3],
+    'metric': ['euclidean', 'manhattan', 'mahalanobis', ],
 }
 
 print('Finding umap embeddings')
-for neighbor in params['n_neighbors']:
-    print(f'Calculating UMAP with {neighbor} neighbors')
+for neighbor, dist, metric in itertools.product(params['n_neighbors'], params['min_dist'], params['metric']):
+    print(f'Calculating UMAP with {neighbor} neighbors, {dist} min dist, metric {metric}')
 
-    # Calculate UMAP
-    umap_df = calc_umap(
-        data=comb.drop('Type', axis=1), # Don't consider type a dimension? Since it may have strong influence on global structure. Not sure though
+    fit = umap.UMAP(
         n_neighbors=neighbor,
+        min_dist=dist,
+        metric=metric,
+        verbose=True,
     )
-    
+
+    umap_df = fit.fit_transform(comb.drop('Type', axis=1));
+
     umap_df = pd.DataFrame(umap_df, index=comb.index)
     umap_df['Type'] = comb['Type'].apply(lambda x: 'Organoid' if x == 1 else 'Primary')
     umap_df = umap_df.rename({0: 'UMAP_1', 1:'UMAP_2'}, axis=1)
@@ -87,14 +82,14 @@ for neighbor in params['n_neighbors']:
     # Generate UMAP plot
     plot_umap(
         umap_df,
-        f'comb_umap_nneigh_{neighbor}'
+        f'comb_umap_nneigh_{neighbor}_{dist}_{metric}'
     )
 
     # Write data to csv
     print('Writing UMAP data to csv')
-    umap_df.to_csv(f'comb_umap_nneigh_{neighbor}.tsv', sep='\t')
+    umap_df.to_csv(f'comb_umap_nneigh_{neighbor}_{dist}_{metric}.tsv', sep='\t')
 
     # Upload data and plots
     print('Uploading UMAP data to S3')
-    upload(f'comb_umap_nneigh_{neighbor}.tsv', f'comb_umap_nneigh_{neighbor}.tsv')
-    upload(f'comb_umap_nneigh_{neighbor}.png', f'comb_umap_nneigh_{neighbor}.png')
+    upload(f'comb_umap_nneigh_{neighbor}_{dist}_{metric}.tsv', f'comb_umap_nneigh_{neighbor}_{dist}_{metric}.tsv')
+    upload(f'comb_umap_nneigh_{neighbor}_{dist}_{metric}.png', f'comb_umap_nneigh_{neighbor}_{dist}_{metric}.png')
