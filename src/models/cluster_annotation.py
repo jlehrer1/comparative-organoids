c5f35e74364c13f5bd87b13cdc27d3a0119234cc
Julian Lehrer <jmlehrer@ucsc.edu>
2022-01-11 22:56:23 -0800

    Added error checking

--- src/models/cluster_annotation.py
+++ src/models/cluster_annotation.py
@@ -28,12 +28,16 @@ for label_file in files:
     for clust in labels.value_counts().index:
         print(f'Calculating for cluster {clust}')
         rows = labels[labels['# label'] == clust].index
-        df = pd.read_csv(os.path.join(path, 'primary.csv'), skiprows = lambda x: x not in rows, names=cols)
-        df.columns = [c.lower() for c in df.columns]
-        df = df.drop(to_remove, axis=1)
-        cluster_df.loc[:, clust] = df.sum().nlargest(1000).index
+        try:
+            df = pd.read_csv(os.path.join(path, 'primary.csv'), skiprows = lambda x: x not in rows, names=cols)
+            df.columns = [c.lower() for c in df.columns]
+            df = df.drop(to_remove, axis=1)
+            cluster_df.loc[:, clust] = df.sum().nlargest(1000).index
+        except Exception as e:
+            print(f'Error with {label_file} in cluster {clust}, continuing...')
+            print(str(e))
         print(cluster_df)
-
+        
     print(f'Saving and uploading top genes for cluster run')
     cluster_df.to_csv(f'annotation_{label_file}')
     upload(
